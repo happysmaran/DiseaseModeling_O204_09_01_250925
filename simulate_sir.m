6ece2f945b302e5fdd37fc4df76b3f852d7c7e63
happysmaran <smaranvallabhaneni@gmail.com>
2025-10-04 15:04:01 -0400

    Add obedience and quarantining values

--- simulate_sir.m
+++ simulate_sir.m
@@ -1,14 +1,15 @@
-function [S, I, R, W] = simulate_sir(s_0, i_0, r_0, m, beta, gamma, num_steps)
+function [S, Q, I, R, W] = simulate_sir(s_0, q_0, i_0, r_0, m, o, beta, gamma, num_steps)
 
 % Setup
 S = zeros(1, num_steps); S(1) = s_0;
+Q = zeros(1, num_steps); Q(1) = q_0;
 I = zeros(1, num_steps); I(1) = i_0;
 R = zeros(1, num_steps); R(1) = r_0;
 W = 1 : num_steps;
 
 % Run simulation
 for step = 1 : (num_steps - 1)
-    [S(step+1), I(step+1), R(step+1)] = helper(S(step), I(step), R(step), m, beta, gamma);
+    [S(step+1), Q(step+1), I(step+1), R(step+1)] = helper(S(step), Q(step), I(step), R(step), m, o, beta, gamma);
 end
 
 end
